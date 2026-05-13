b7830af523ad9ebac90b2277ccf4e7a0c60ccf05
Misaf <misaf.1990@gmail.com>
2026-05-13 13:07:57 +0330

    feat: add German translations

---
+++ resources/lang/de/navigation.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'multimedia'            => 'Multimedia',
+    'multimedia_management' => 'Multimedia-Verwaltung',
+];
---
+++ resources/lang/de/tables.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'model_type'             => 'Modelltyp',
+    'model_id'               => 'Modell-ID',
+    'uuid'                   => 'UUID',
+    'collection_name'        => 'Sammlungsname',
+    'name'                   => 'Name',
+    'file_name'              => 'Dateiname',
+    'mime_type'              => 'MIME-Typ',
+    'disk'                   => 'Datenträger',
+    'conversions_disk'       => 'Konvertierungsdatenträger',
+    'size'                   => 'Größe',
+    'manipulations'          => 'Bearbeitungen',
+    'custom_properties'      => 'Benutzerdefinierte Eigenschaften',
+    'gernerated_conversions' => 'Generierte Konvertierungen',
+    'responsive_images'      => 'Responsive Bilder',
+    'order_column'           => 'Sortierspalte',
+    'created_at'             => 'Erstellt am',
+    'updated_at'             => 'Aktualisiert am',
+];
