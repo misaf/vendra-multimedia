1410c83f52406fedac54c21eecd950a1ddecd614
Misaf <misaf.1990@gmail.com>
2026-06-06 13:21:27 +0330

    closed #1: Laravel 13: Replace hidden with PHP Attributes

--- src/Models/Multimedia.php
+++ src/Models/Multimedia.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Misaf\VendraMultimedia\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Misaf\VendraTenant\Traits\BelongsToTenant;
 use Spatie\MediaLibrary\MediaCollections\Models\Media as SpatieMediaLibrary;
 
@@ -12,13 +13,10 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media as SpatieMediaLibrary;
  *
  * @property int $tenant_id
  */
+#[Hidden(['tenant_id'])]
 final class Multimedia extends SpatieMediaLibrary
 {
     use BelongsToTenant;
-
-    protected $hidden = [
-        'tenant_id',
-    ];
 
     protected function casts(): array
     {
