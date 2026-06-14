89b43037cdc07d9c5bf0085cebd4e4d51e1c8f57
Misaf <misaf.1990@gmail.com>
2026-06-14 13:12:59 +0330

    Clean up Multimedia model docblock

--- src/Models/Multimedia.php
+++ src/Models/Multimedia.php
@@ -9,8 +9,6 @@ use Misaf\VendraTenant\Traits\BelongsToTenant;
 use Spatie\MediaLibrary\MediaCollections\Models\Media as SpatieMediaLibrary;
 
 /**
- * Misaf\VendraMultimedia\Models\Multimedia.
- *
  * @property int $tenant_id
  */
 #[Hidden(['tenant_id'])]
