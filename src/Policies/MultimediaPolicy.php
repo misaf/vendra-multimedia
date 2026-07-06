0bd82eae565d1598814588da399a214f3b367158
Misaf <misaf.1990@gmail.com>
2026-07-06 13:06:32 +0330

    Clean up module autoload and policies

--- src/Policies/MultimediaPolicy.php
+++ src/Policies/MultimediaPolicy.php
@@ -5,66 +5,66 @@ declare(strict_types=1);
 namespace Misaf\VendraMultimedia\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Misaf\VendraMultimedia\Enums\MultimediaPolicyEnum;
 use Misaf\VendraMultimedia\Models\Multimedia;
-use Misaf\VendraUser\Models\User;
 
 final class MultimediaPolicy
 {
     use HandlesAuthorization;
 
-    public function create(User $user): bool
+    public function create(Authorizable $user): bool
     {
-        return $user->can(MultimediaPolicyEnum::CREATE);
+        return $user->can(MultimediaPolicyEnum::CREATE->value);
     }
 
-    public function delete(User $user, Multimedia $multimedia): bool
+    public function delete(Authorizable $user, Multimedia $multimedia): bool
     {
-        return $user->can(MultimediaPolicyEnum::DELETE);
+        return $user->can(MultimediaPolicyEnum::DELETE->value);
     }
 
-    public function deleteAny(User $user): bool
+    public function deleteAny(Authorizable $user): bool
     {
-        return $user->can(MultimediaPolicyEnum::DELETE_ANY);
+        return $user->can(MultimediaPolicyEnum::DELETE_ANY->value);
     }
 
-    public function forceDelete(User $user, Multimedia $multimedia): bool
+    public function forceDelete(Authorizable $user, Multimedia $multimedia): bool
     {
-        return $user->can(MultimediaPolicyEnum::FORCE_DELETE);
+        return $user->can(MultimediaPolicyEnum::FORCE_DELETE->value);
     }
 
-    public function forceDeleteAny(User $user): bool
+    public function forceDeleteAny(Authorizable $user): bool
     {
-        return $user->can(MultimediaPolicyEnum::FORCE_DELETE_ANY);
+        return $user->can(MultimediaPolicyEnum::FORCE_DELETE_ANY->value);
     }
 
-    public function replicate(User $user, Multimedia $multimedia): bool
+    public function replicate(Authorizable $user, Multimedia $multimedia): bool
     {
-        return $user->can(MultimediaPolicyEnum::REPLICATE);
+        return $user->can(MultimediaPolicyEnum::REPLICATE->value);
     }
 
-    public function restore(User $user, Multimedia $multimedia): bool
+    public function restore(Authorizable $user, Multimedia $multimedia): bool
     {
-        return $user->can(MultimediaPolicyEnum::RESTORE);
+        return $user->can(MultimediaPolicyEnum::RESTORE->value);
     }
 
-    public function restoreAny(User $user): bool
+    public function restoreAny(Authorizable $user): bool
     {
-        return $user->can(MultimediaPolicyEnum::RESTORE_ANY);
+        return $user->can(MultimediaPolicyEnum::RESTORE_ANY->value);
     }
 
-    public function update(User $user, Multimedia $multimedia): bool
+    public function update(Authorizable $user, Multimedia $multimedia): bool
     {
-        return $user->can(MultimediaPolicyEnum::UPDATE);
+        return $user->can(MultimediaPolicyEnum::UPDATE->value);
     }
 
-    public function view(User $user, Multimedia $multimedia): bool
+    public function view(Authorizable $user, Multimedia $multimedia): bool
     {
-        return $user->can(MultimediaPolicyEnum::VIEW);
+        return $user->can(MultimediaPolicyEnum::VIEW->value);
     }
 
-    public function viewAny(User $user): bool
+    public function viewAny(Authorizable $user): bool
     {
-        return $user->can(MultimediaPolicyEnum::VIEW_ANY);
+        return $user->can(MultimediaPolicyEnum::VIEW_ANY->value);
     }
 }
