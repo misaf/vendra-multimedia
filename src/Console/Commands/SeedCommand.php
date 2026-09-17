440f3380b96ca35116d160097c87fee5eae47436
misaf <misaf.1990@gmail.com>
2026-09-17 19:49:53 +0000

    test(store): cover the storefront:lifecycle command

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 namespace Misaf\VendraMultimedia\Console\Commands;
 
 use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
 use Misaf\VendraMultimedia\Database\Seeders\PermissionPolicySeeder;
 use Misaf\VendraMultimedia\MultimediaPlugin;
 use Misaf\VendraSupport\Tenancy\Console\Commands\TenantSeedCommand;
 
 #[Description('Seed multimedia module data for a tenant')]
+#[Signature(self::MODULE_NAME.':seed
+        {tenant? : Tenant ID or slug to seed multimedia data for}
+        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies}')]
 final class SeedCommand extends TenantSeedCommand
 {
     protected const string MODULE_NAME = MultimediaPlugin::ID;
-
-    protected $signature = self::MODULE_NAME.':seed
-        {tenant? : Tenant ID or slug to seed multimedia data for}
-        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies}';
 
     /**
      * @return array<string, class-string>
